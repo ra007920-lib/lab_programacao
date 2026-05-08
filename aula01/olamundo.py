8128096f3bd517580f6573a4fe91dee44fa1c86d
Iago Montanhini <007920@libertas.edu.br>
2026-05-07 21:11:35 -0300

    primeira aula de python

---
+++ aula01/olamundo.py
@@ -0,0 +1,8 @@
+# com o hashtag a gente não executa a linha (é um comentário)
+print('Hello, World!') # o comentário não é lido
+print('aprendendo a linguagem python') #print indica que vamos imprimir
+print('meu nome é iago')
+''' usando 3 aspas ele também é comentário
+mas nesse caso posso dar parágrafo.'''
+# em python "=" dá valor à uma variável. exemplo.
+nome = "Iago"
---
+++ aula01/triangulo.py
@@ -0,0 +1,5 @@
+#este programa calcula a área de um triângulo.
+base = int(input("insira o valor da base: "))
+altura = int(input("insira o valor da altura: "))
+area = (base * altura) / 2
+print(f"o valor da área do triângulo será {area}")
